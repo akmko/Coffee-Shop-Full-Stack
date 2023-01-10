75051c5dec184192957ea8eb10ed000ad8098ec1
akmko <111931776+akmko@users.noreply.github.com>
2023-01-10 10:26:25 +0900

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -6,10 +6,12 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'moc.jp.auth0.com', // the auth0 domain prefix
+    url: 'moc.jp', // the auth0 domain prefix
     audience: 'http://localhost:5000', // the audience set for the auth0 app
     clientId: 'gBovIqxhH6ghFcMbLRFjPM1VneUcTSaN', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://localhost:5000/login-results', // the base url of the running ionic application. 
   }
 };
+
+
 
